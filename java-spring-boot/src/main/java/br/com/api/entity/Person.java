e8aaf903ab1249cef0ad171c4876e0aeca2eba24
Guilherme <guilhermemartinazzo@hotmail.com>
2023-11-24 17:19:36 -0300

    criando cadastro de pessoa

--- java-spring-boot/src/main/java/br/com/api/entity/Person.java
+++ java-spring-boot/src/main/java/br/com/api/entity/Person.java
@@ -1,16 +1,56 @@
 package br.com.api.entity;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
 import java.util.Objects;
 
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "person")
 public class Person implements Serializable {
 
 	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	@Column(name = "first_name", nullable = false)
 	private String firstName;
+	@Column(name = "last_name")
 	private String lastName;
+	@Column
 	private String address;
+	@Column
 	private String gender;
+
+	@Column(name = "created_at", updatable = false, nullable = false)
+	@CreationTimestamp
+	@JsonFormat(pattern = "YYYY-MM-dd HH:mm:ss")
+	private LocalDateTime createdAt;
+
+	@Column(name = "updated_at", insertable = false)
+	@UpdateTimestamp
+	@JsonFormat(pattern = "YYYY-MM-dd HH:mm:ss")
+	private LocalDateTime updatedAt;
+
+	public LocalDateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	public LocalDateTime getUpdatedAt() {
+		return updatedAt;
+	}
 
 	public Long getId() {
 		return id;
@@ -54,7 +94,7 @@ public class Person implements Serializable {
 
 	@Override
 	public int hashCode() {
-		return Objects.hash(address, firstName, gender, id, lastName);
+		return Objects.hash(address, createdAt, firstName, gender, id, lastName, updatedAt);
 	}
 
 	@Override
@@ -66,9 +106,10 @@ public class Person implements Serializable {
 		if (getClass() != obj.getClass())
 			return false;
 		Person other = (Person) obj;
-		return Objects.equals(address, other.address) && Objects.equals(firstName, other.firstName)
-				&& Objects.equals(gender, other.gender) && Objects.equals(id, other.id)
-				&& Objects.equals(lastName, other.lastName);
+		return Objects.equals(address, other.address) && Objects.equals(createdAt, other.createdAt)
+				&& Objects.equals(firstName, other.firstName) && Objects.equals(gender, other.gender)
+				&& Objects.equals(id, other.id) && Objects.equals(lastName, other.lastName)
+				&& Objects.equals(updatedAt, other.updatedAt);
 	}
 
 }
--- java-spring-boot/src/main/java/br/com/api/service/PersonService.java
+++ java-spring-boot/src/main/java/br/com/api/service/PersonService.java
@@ -1,54 +1,61 @@
 package br.com.api.service;
 
 import java.util.List;
-import java.util.concurrent.atomic.AtomicLong;
 import java.util.logging.Logger;
 
 import org.springframework.stereotype.Service;
 
 import br.com.api.entity.Person;
+import br.com.api.exception.ResourceNotFoundException;
+import br.com.api.repository.PersonRepository;
+import jakarta.transaction.Transactional;
 
 @Service
 public class PersonService {
 
-	private final AtomicLong counter = new AtomicLong();
 	private Logger logger = Logger.getLogger(PersonService.class.getName());
+	private final PersonRepository personRepository;
+
+	PersonService(PersonRepository personRepository) {
+		this.personRepository = personRepository;
+	}
 
 	public Person findById(Long id) {
 		logger.info("Finding person by id");
-		Person person = buildPersonMock();
-		return person;
+		return personRepository.findById(id).orElseThrow(() -> new ResourceNotFoundException("Person Not Found"));
 	}
 
 	public List<Person> findAll() {
 		logger.info("Finding all people");
-		return List.of(buildPersonMock(), buildPersonMock(), buildPersonMock(), buildPersonMock(), buildPersonMock(),
-				buildPersonMock(), buildPersonMock(), buildPersonMock());
+		return personRepository.findAll();
 
 	}
 
+	@Transactional
 	public Person create(Person person) {
 		logger.info("Creating a person");
-		return person;
+		return personRepository.save(person);
+		
 	}
 
+	@Transactional
 	public Person update(Person person) {
 		logger.info("Updating a person");
-		return person;
+		var personEntity = personRepository.findById(person.getId())
+				.orElseThrow(() -> new ResourceNotFoundException("Person Not Found"));
+		personEntity.setFirstName(person.getFirstName());
+		personEntity.setLastName(person.getLastName());
+		personEntity.setAddress(person.getAddress());
+		personEntity.setGender(person.getGender());
+		Person save = personRepository.save(personEntity);
+		return save;
 	}
 
 	public void delete(Long id) {
 		logger.info("Deleting a person");
+		var personEntity = personRepository.findById(id)
+				.orElseThrow(() -> new ResourceNotFoundException("Person Not Found"));
+		personRepository.delete(personEntity);
 	}
 
-	private Person buildPersonMock() {
-		Person person = new Person();
-		long id = counter.incrementAndGet();
-		person.setId(id);
-		person.setFirstName("Person name " + id);
-		person.setLastName("Last name " + id);
-		person.setAddress("Some address in Brazil " + id);
-		person.setGender("Male");
-		return person;
-	}
 }
