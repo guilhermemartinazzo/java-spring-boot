4cb44fd5324b95e4bcf60842ccf5f44aba9db679
Guilherme <guilhermemartinazzo@hotmail.com>
2024-02-13 14:28:13 -0300

    feat: adding flyway

---
+++ java-spring-boot/src/main/resources/db/migration/V1__Create_table_person.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS `person` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `first_name` varchar(255) NOT NULL,
+  `last_name` varchar(255) DEFAULT NULL,
+  `address` varchar(255) DEFAULT NULL,
+  `gender` varchar(255) DEFAULT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+);
+
---
+++ java-spring-boot/src/main/resources/db/migration/V2__Inserts_table_person.sql
@@ -0,0 +1,9 @@
+INSERT INTO `person` (`id`,`address`,`first_name`,`gender`,`last_name`,`created_at`,`updated_at`) VALUES
+(9,'Endereço teste','Guilherme','Male','Martinazzo','2023-12-03 19:36:46.631387','2023-12-03 19:37:40.310748')
+,(10,'Endereço 2','Guilherme 2','Male','Martinazzo 2','2023-12-03 20:04:43.486962',NULL)
+,(11,'rua x','eu','Male','santos','2024-01-12 15:28:34.807141',NULL)
+,(12,'rua x2','e2u','Male','santos2','2024-01-12 15:35:26.085001',NULL)
+,(13,'rua x3','eu3','Male','santos3','2024-01-12 16:03:08.921980',NULL)
+,(14,'rua x3','eu3','Male','santos3','2024-01-12 16:03:27.070512',NULL)
+,(15,'rua x3','eu3','Male','santos3','2024-01-12 16:03:31.893140',NULL);
+
