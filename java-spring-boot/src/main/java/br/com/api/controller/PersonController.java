8f4ae04c61e5a340fa977910db20356275294cad
Guilherme <guilhermemartinazzo@hotmail.com>
2024-02-13 16:42:17 -0300

    feat: Adding xml and yaml content types to the api services

--- java-spring-boot/src/main/java/br/com/api/controller/PersonController.java
+++ java-spring-boot/src/main/java/br/com/api/controller/PersonController.java
@@ -1,67 +1,62 @@
 package br.com.api.controller;
 
-import java.util.List;
-
-import org.springframework.http.HttpStatus;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
-
 import br.com.api.service.PersonService;
+import br.com.api.utils.MediaTypeUtils;
 import br.com.api.vo.PersonVO;
 import br.com.api.vo.v2.PersonVO2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
-@RequestMapping(value = "/person")
+@RequestMapping(value = "/api/person")
 public class PersonController {
 
-	private final PersonService personService;
+    private final PersonService personService;
 
-	PersonController(PersonService service) {
-		this.personService = service;
-	}
+    PersonController(PersonService service) {
+        this.personService = service;
+    }
 
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE, value = "/{personId}")
-	public ResponseEntity<PersonVO> findById(@PathVariable(name = "personId") Long personId) {
-		return ResponseEntity.ok(personService.findById(personId));
-	}
+    @GetMapping(produces = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YML}, value = "/{personId}")
+    public ResponseEntity<PersonVO> findById(@PathVariable(name = "personId") Long personId) {
+        return ResponseEntity.ok(personService.findById(personId));
+    }
 
-	@GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<List<PersonVO>> findAll() {
-		return ResponseEntity.ok(personService.findAll());
-	}
+    @GetMapping(produces = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YML})
+    public ResponseEntity<List<PersonVO>> findAll() {
+        return ResponseEntity.ok(personService.findAll());
+    }
 
-	@PostMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<PersonVO> create(@RequestBody PersonVO person) {
-		return ResponseEntity.ok(personService.create(person));
-	}
+    @PostMapping(produces = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YML},
+            consumes = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YML})
+    public ResponseEntity<PersonVO> create(@RequestBody PersonVO person) {
+        return ResponseEntity.ok(personService.create(person));
+    }
 
-	@PutMapping(produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<PersonVO> update(@RequestBody PersonVO person) {
-		return ResponseEntity.ok(personService.update(person));
-	}
+    @PutMapping(produces = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YML},
+            consumes = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YML})
+    public ResponseEntity<PersonVO> update(@RequestBody PersonVO person) {
+        return ResponseEntity.ok(personService.update(person));
+    }
 
-	@DeleteMapping("/{personId}")
-	@ResponseStatus(code = HttpStatus.NO_CONTENT)
-	public void update(@PathVariable(value = "personId") Long personId) {
-		personService.delete(personId);
-	}
-	
-	/**
-	 * V2
-	 */
-	
-	@PostMapping(value = "/v2", produces = MediaType.APPLICATION_JSON_VALUE, consumes = MediaType.APPLICATION_JSON_VALUE)
-	public ResponseEntity<PersonVO2> createV2(@RequestBody PersonVO2 person) {
-		return ResponseEntity.ok(personService.createV2(person));
-	}
-	
+    @DeleteMapping("/{personId}")
+    @ResponseStatus(code = HttpStatus.NO_CONTENT)
+    public void update(@PathVariable(value = "personId") Long personId) {
+        personService.delete(personId);
+    }
+
+    /**
+     * V2
+     */
+
+    @PostMapping(value = "/v2",
+            produces = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YML},
+            consumes = {MediaTypeUtils.APPLICATION_JSON, MediaTypeUtils.APPLICATION_XML, MediaTypeUtils.APPLICATION_YML})
+    public ResponseEntity<PersonVO2> createV2(@RequestBody PersonVO2 person) {
+        return ResponseEntity.ok(personService.createV2(person));
+    }
+
 }
---
+++ java-spring-boot/src/main/java/br/com/api/utils/MediaTypeUtils.java
@@ -0,0 +1,8 @@
+package br.com.api.utils;
+
+public class MediaTypeUtils {
+    public static final String APPLICATION_YML = "application/x-yaml";
+    public static final String APPLICATION_XML = "application/xml";
+    public static final String APPLICATION_JSON = "application/json";
+
+}
